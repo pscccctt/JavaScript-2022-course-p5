d6dba7c2b8be28aed43ec99d7521228237df8c60
pscccctt <pscccctt@gmail.com>
2023-01-07 12:30:11 +0100

    Cleaned up controller again

--- src/js/controller.js
+++ src/js/controller.js
@@ -128,7 +128,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  newFeature();
 };
 
 init();
